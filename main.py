e00004628fa864e5d1c16e9340daa928aa39cfb3
SuilenStudios <contact@willemsuilen.com>
2023-11-21 19:02:57 +0100

    change eval question

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     peft = Peft(llm, "Fransver/zephyr-hboi-sb")
     controller = LlmController(llm, peft)
     controller.configure_llm()
-    llm_response = controller.run("What is the meaning of life?")
+    llm_response = controller.run("How can I set up a version control system for our team project to achieve the KPI with the description: 'Manage personal files and the configuration of these files in a software development environment??")
