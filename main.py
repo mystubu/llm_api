1c5cfbb13a3da07d5d18d36e7f24b74e99bab785
BOSSyouri <yourilinders@hotmail.com>
2023-11-21 19:28:15 +0100

    namechange

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 from llm import Llm
-from peft import Peft
+from lora import Lora
 from llm_controller import LlmController
-import torch
+# import torch
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/ask/{question}")
-def ask_llm(question: str):
-    return {"question": question,
-        "answer": "answer"}
+# @app.get("/ask/{question}")
+# def ask_llm(question: str):
+#     return {"question": question,
+#         "answer": "answer"}
 
 if __name__ == "__main__":
     llm = Llm("HuggingFaceH4/zephyr-7b-beta")
-    peft = Peft(llm, "Fransver/zephyr-hboi-sb")
+    peft = Lora(llm, "Fransver/zephyr-hboi-sb")
     controller = LlmController(llm, peft)
     controller.configure_llm()
     llm_response = controller.run("How can I set up a version control system for our team project to achieve the KPI with the description: 'Manage personal files and the configuration of these files in a software development environment??")
+    print(llm_response)
