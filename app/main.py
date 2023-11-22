48d53f6fcd533b726e84b3f734a7e0112b568689
wsuilen <suilenwillem@gmail.com>
2023-11-22 13:29:21 +0000

    init dockerfile and setting up folder structure

--- app/main.py
+++ app/main.py
@@ -27,6 +27,6 @@ app = FastAPI(lifespan=lifespan)
 def ask_llm(question: str):
     processor = QueryProcessor(question, llm, tokenizer, lora)
     answer = processor.run()
-    return answer
+    return {"answer": answer}
 
 # prompt = "How can I set up a version control system for our team project to achieve the KPI with the description: 'Manage personal files and the configuration of these files in a software development environment?"
