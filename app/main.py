7c536f46c2053bad6d4b7ce2fb673a2cde072989
SuilenStudios <contact@willemsuilen.com>
2023-11-28 13:44:29 +0100

    clean up dependencies

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from contextlib import asynccontextmanager
-# from app.llm_configurator import LlmConfigurator
-# from app.query_processor import QueryProcessor
-# from app.question import Question
-# from llm_configurator import LlmConfigurator
-# from query_processor import QueryProcessor
-# from question import Question
+from app.llm_configurator import LlmConfigurator
+from app.query_processor import QueryProcessor
+from app.question import Question
 import app.authenticator as authenticator
 from fastapi import HTTPException
 
@@ -16,22 +13,22 @@ lora = None
 tokenizer = None
 question = None
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     global configurator, llm, lora, tokenizer    
-#     configurator = LlmConfigurator()
-#     llm = configurator.get_llm()
-#     lora = configurator.get_lora()
-#     tokenizer = configurator.get_tokenizer()
-#     yield
-#     configurator = None
-#     llm = None
-#     lora = None
-#     tokenizer = None
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global configurator, llm, lora, tokenizer    
+    configurator = LlmConfigurator()
+    llm = configurator.get_llm()
+    lora = configurator.get_lora()
+    tokenizer = configurator.get_tokenizer()
+    yield
+    configurator = None
+    llm = None
+    lora = None
+    tokenizer = None
 
 
-# app = FastAPI(lifespan=lifespan)
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+# app = FastAPI()
 
 @app.post("/token", response_model=authenticator.Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -49,16 +46,16 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 async def read_users_me(current_user: authenticator.User = Depends(authenticator.get_current_user)):
     return current_user
 
-# @app.post("/question/")
-# async def create_question(q: Question):
-#     global question 
-#     question = q
-#     return q
+@app.post("/question/")
+async def create_question(q: Question):
+    global question 
+    question = q
+    return q
 
-# @app.get("/ask/{question}")
-# async def ask_llm(question: str):
-#     processor = QueryProcessor(question.question, llm, tokenizer, lora)
-#     answer = processor.run()
-#     return {"answer": answer}
+@app.get("/ask/{question}")
+async def ask_llm(question: str):
+    processor = QueryProcessor(question.question, llm, tokenizer, lora)
+    answer = processor.run()
+    return {"answer": answer}
 
 # prompt = "How can I set up a version control system for our team project to achieve the KPI with the description: 'Manage personal files and the configuration of these files in a software development environment?"
