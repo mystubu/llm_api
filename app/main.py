91d8f6a5086b849e0c69c64a6976e78e4e8d879f
Verberne,Frans F.P.M. <f.verberne@student.fontys.nl>
2023-11-30 12:21:44 +0100

    :art: added ping method testing and path update docker

--- app/main.py
+++ app/main.py
@@ -14,6 +14,9 @@ def ask_llm(job):
     answer = processor.run()
     return {"answer": answer}
 
+def ping(job):
+    return {"message": "pong"}
+
 # prompt = "How can I set up a version control system for our team project to achieve the KPI with the description: 'Manage personal files and the configuration of these files in a software development environment?"
 
 runpod.serverless.start({"handler": ask_llm})
